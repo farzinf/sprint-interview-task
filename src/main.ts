48d919e67f426967c535f1b5812a1a464ec43853
farzin <farzin.falahati@gmail.com>
2024-12-25 18:18:08 +0330

    set nest app type as express platform

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swaggerSetup } from './swagger';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   swaggerSetup(app);
   await app.listen(3000);
 }
