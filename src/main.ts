97de5a71c0c2dfe2cdaef1537769fc7fd66a8caf
farzin <farzin.falahati@gmail.com>
2024-12-25 18:26:30 +0330

    handle validation error of DTOs

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,20 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { swaggerSetup } from './swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
+import { ValidationError } from './modules/common/errors';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      exceptionFactory: (errors) => new ValidationError(errors),
+    }),
+  );
+
   swaggerSetup(app);
   await app.listen(3000);
 }
