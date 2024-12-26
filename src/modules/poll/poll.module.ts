8626c9ca8e610972743fd47246fc9959b7d71ab6
farzin <farzin.falahati@gmail.com>
2024-12-26 08:47:57 +0330

    provide poll gateway in poll module

--- src/modules/poll/poll.module.ts
+++ src/modules/poll/poll.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Poll, Vote, PollOption } from './entities';
 import { PollController } from './poll.controller';
 import { PollService } from './poll.service';
+import { PollGateway } from './poll.gateway';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Poll, PollOption, Vote])],
   controllers: [PollController],
-  providers: [PollService],
+  providers: [PollService, PollGateway],
   exports: [PollService],
 })
 export class PollModule {}
